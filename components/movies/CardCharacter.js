e2b764865b6ffaeba5d255290a3083a88ff3b970
unknown <edison>
2023-04-19 23:22:53 -0500

    fix: hair

--- components/movies/CardCharacter.js
+++ components/movies/CardCharacter.js
@@ -66,6 +66,7 @@ export default function CardCharacter(props) {
                 <ListGroup.Item><strong>Altura: </strong>{props.character.height}</ListGroup.Item>
                 <ListGroup.Item><strong>Peso: </strong>{props.character.mass}</ListGroup.Item>
                 <ListGroup.Item><strong>Color de cabello: </strong>{props.character.hair_color}</ListGroup.Item>
+
                 <ListGroup.Item><strong>Año de nacimiento: </strong>{props.character.birth_year}</ListGroup.Item>
                 <ListGroup.Item><strong>Planeta: </strong>{planet.name}</ListGroup.Item>
                 <ListGroup.Item>
